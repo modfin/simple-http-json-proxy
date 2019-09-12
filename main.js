988b9b226e9258309e18605ea59637fe6e11a606
jz-modfin <jonas.zeitler@modularfinance.se>
2019-09-12 10:25:56 +0200

    fix cors + proxy sub request error handling

--- main.js
+++ main.js
@@ -2,20 +2,34 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 let server = http.createServer((req, res) => {
-    proxyUrl = req.url.replace('/', '');
-    parsedUrl = url.parse(proxyUrl);
-    console.info(`proxying ${proxyUrl}`)
+    const queryObject = url.parse(req.url, true).query;
+    const proxyUrl = queryObject.url;
+    const parsedUrl = url.parse(proxyUrl || '');
+    console.info(`proxying ${proxyUrl}`);
+
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+
     const handler = (proxyRes) => {
-        proxyRes.pipe(res, {end: true});
+        proxyRes.pipe(res, { end: true });
         res.setHeader('content-type', 'application/json');
     }
-    if (parsedUrl.protocol === 'https:' || parsedUrl.protocol === 'http:') {
+    if (
+        (parsedUrl.protocol === 'https:' || parsedUrl.protocol === 'http:')
+        && typeof parsedUrl.hostname === 'string'
+        && typeof parsedUrl.path === 'string'
+    ) {
         let proxy = parsedUrl.protocol === 'https:'
-            ? https.request(proxyUrl, { method: 'GET' }, handler)
-            : http.request(proxyUrl, { method: 'GET' }, handler);
-        req.pipe(proxy, {end: true});
+            ? https.request({ method: 'GET', ...parsedUrl }, handler)
+            : http.request({ method: 'GET', ...parsedUrl }, handler);
+        proxy.on('error', (e) => {
+            res.writeHead(400, { 'Content-Type': 'text/plain; charset=UTF-8' });
+            res.write(`proxy request '${proxyUrl}' error: ${e.message}`);
+            res.end();
+        })
+        req.pipe(proxy, { end: true });
     } else {
-        res.writeHead(400, {'Content-Type': 'text/plain; charset=UTF-8'});
+        res.writeHead(400, { 'Content-Type': 'text/plain; charset=UTF-8' });
         res.write(`proxy url must be a valid url. expected: 'http[s]://host.tld/path/to/resource', actual: '${proxyUrl}'`);
         res.end();
     }
--- readme.md
+++ readme.md
@@ -9,11 +9,11 @@ node main.js
 
 use:
 
-Make a http GET request to `http://localhost:7357/http[s]://host.tld/path/to/resource` to get the json response of `http[s]://host.tld/path/to/resource` back.
+Make a http GET request to `http://localhost:7357?url=http[s]://host.tld/path/to/resource` to get the json response of `http[s]://host.tld/path/to/resource` back.
 
 examples:
 ```
-curl http://localhost:7357/http://fx.modfin.se/latest
+curl http://localhost:7357?url=http://fx.modfin.se/latest
 ```
 
-or visit `http://localhost:7357/http://fx.modfin.se/latest` in a web browser.
+or visit `http://localhost:7357?url=http://fx.modfin.se/latest` in a web browser.
